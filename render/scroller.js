db2a5b8e5997f663615a83a54df900e31a35c47d
0-l <0th@protonmail.com>
2018-12-30 17:18:23 -0200

    added vim scrolling bindings

---
+++ render/scroller.js
@@ -0,0 +1,23 @@
+class Scroller {
+  constructor (key, bindings) {
+    this.panel = $('#files[active]');
+
+    for (let _key_ in bindings)
+      if (_key_ == key)
+        this.scroll(eval(bindings[key]));
+  }
+
+  get height () {
+    return this.panel.offsetHeight;
+  }
+
+  percentage (times) {
+    return (times / 100) * this.height;
+  }
+
+  scroll (value) {
+    this.panel.scrollBy(0, this.percentage(value));
+  }
+}
+
+module.exports = Scroller;
