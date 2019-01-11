ff187c8387e08bab449632cabe05006b10223d2b
0-l <0th@protonmail.com>
2019-01-11 11:57:27 -0200

    tweaked the search bar a little

--- render/search.js
+++ render/search.js
@@ -1,13 +1,33 @@
 // TODO: maybe add the input above the sorting panel
+// TODO: parse 'ESC' for closing the search panel
+// TODO: add 'active' state to the search nav button
 
 class Search {
   constructor () {
-    $('.control .search').onclick = () =>
+    this.input = $('#search input');
+
+    $('.search').onclick = () =>
       this.toggle();
   }
 
   toggle () {
     $('#search').classList.toggle('active');
+
+    this.input.focus();
+  }
+
+  isActive () {
+    return this.input == document.activeElement;
+  }
+
+  clear() {
+    $('.filter').innerHTML     = '';
+    $('.list').style.display   = 'block';
+    $('.marker').style.opacity = 0;
+  }
+
+  empty (elem) {
+    return /^[\s\n\r]$/m.test(elem) || elem.length === 0;
   }
 }
 
